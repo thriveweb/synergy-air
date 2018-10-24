7fae7dca90fc951e67c2302cc36d359b8cd3d8fe
Sam <sam@thriveweb.com.au>
2018-10-24 10:43:19 +1000

    page header update

--- src/components/PageHeader.js
+++ src/components/PageHeader.js
@@ -2,27 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Image from './Image'
-import Content from './Content'
 import './PageHeader.css'
 
-const PageHeader = ({
-  title,
-  subtitle,
-  backgroundImage,
-  large,
-  className = ''
-}) => {
-  if (large) className += ' PageHeader-large'
+const PageHeader = ({ title, backgroundImage, large, className = '' }) => {
+  if (large) className += ' pageHeader-large'
   return (
-    <div className={`PageHeader relative ${className}`}>
+    <div className={`pageHeader relative ${className}`}>
       {backgroundImage && (
         <Image background src={backgroundImage} alt={title} size="cover" />
       )}
-      <div className="container relative">
-        <h1 className="PageHeader--Title">{title}</h1>
-        {subtitle && (
-          <Content className="PageHeader--Subtitle" src={subtitle} />
-        )}
+      <div className="wide relative">
+        <h1>{title}</h1>
       </div>
     </div>
   )
