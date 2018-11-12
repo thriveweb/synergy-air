83cef9bb47b67a61666c422e17da9f6f1b853efd
Francesca <francesca@thrivewb.com.au>
2018-11-12 13:40:06 +1000

    fix portals map

--- src/components/Portals.js
+++ src/components/Portals.js
@@ -6,30 +6,31 @@ import './Portals.css'
 
 export default ({ portals }) => (
   <div className="portals">
-    {portals & !!portals.length &&
-      portals.map((item, index) => (
-        <div className="half" key={index}>
-          <div className="item">
-            <Link to={item.productLink} className="absolute link">
-              <div className="details">
-                <h3>
-                  <strong>Synergy Air</strong> {item.title}
-                </h3>
-                <h5>View Products</h5>
-              </div>
-            </Link>
-            <Image
-              src={item.image}
-              alt={item.title}
-              className="cover"
-              resolutions="large"
-            />
-          </div>
-
-          <Link to={item.distLink} className="button">
-            View distributors
+  { portals &&
+    !!portals.length &&
+    portals.map((item, index) => {
+      <div className="half" key={index}>
+        <div className="item">
+          <Link to={item.productLink} className="absolute link">
+            <div className="details">
+              <h3>
+                <strong>Synergy Air</strong> {item.title}
+              </h3>
+              <h5>View Products</h5>
+            </div>
           </Link>
+          <Image
+            src={item.image}
+            alt={item.title}
+            className="cover"
+            resolutions="large"
+          />
         </div>
-      ))}
+
+        <Link to={item.distLink} className="button">
+          View distributors
+        </Link>
+      </div>
+    ))}
   </div>
 )
